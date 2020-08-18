f7b292a77d53c039853962cb98b7521ebe6b8b63
Alma Flores <flores.almalee@gmail.com>
2020-08-18 20:55:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,8 @@ def oxford_comma(array)
   elsif array.size == 2 
     array.join(" and ")
   elsif array.size == 3 
-    array.join(',').reverse.sub(',', ' and ').reverse
+    array.join(',').reverse.sub(' , ', ' and ').reverse
   else
-    array.join(',').reverse.sub(',', ' and ').reverse
+    array.join(',').reverse.sub(' , ', ' and ').reverse
   end
 end
