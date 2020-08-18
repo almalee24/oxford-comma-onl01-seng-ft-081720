b1ff09f81a57de5e74ee69b1a7c05c0c6592ccb6
Alma Flores <flores.almalee@gmail.com>
2020-08-18 20:46:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.size == 1
     array.join
   elsif array.size == 2 
-    puts array.join('and')
+    array.join('and')
   elsif array.size == 3 
     array.join(',')
     array[array.size - 1] = "and"
